bb57bf4bc9c78c64e818272bb154e96b5d406acf
TobySlump <toby.slump@gmail.com>
2024-09-10 10:01:34 +0100

    Opponent Options Input

--- Open Energy Market Technical Assessment/src/Main.java
+++ Open Energy Market Technical Assessment/src/Main.java
@@ -12,6 +12,15 @@ public class Main {
         // Set up reader to obtain user inputs.
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 
+        // Set up player and computer objects.
+        Player player = new Player();
+        OriginalComputer originalComputer = new OriginalComputer();
+        LastChoiceComputer lastChoiceComputer = new LastChoiceComputer();
+
+        // Set up game parameters.
+        String gameMode = null;
+        String opponent = null;
+
         System.out.println("Hello and welcome to my Rock Paper Scissors game!");
 
         boolean keepPlaying = true;
@@ -27,7 +36,7 @@ public class Main {
             // Repeat game mode question until valid answer provided.
             while (!validGameMode) {
                 System.out.println("Enter the game mode you would like to play (1, 2, 3): ");
-                String gameMode = reader.readLine();
+                gameMode = reader.readLine();
 
                 if (Objects.equals(gameMode, "1")
                         || Objects.equals(gameMode, "2")
@@ -35,6 +44,37 @@ public class Main {
                     validGameMode = true;
                 }
             }
+
+            // Exit keepPlaying loop preemptively if "Quit" option is chosen.
+            if (gameMode.equals("3")){
+                break;
+            }
+
+            System.out.println("""
+                Opponent Options:\s
+                1) Random Computer.\s
+                2) Last Choice Computer""");
+
+            boolean validOpponent = false;
+
+            // Repeat opponent question until valid answer provided.
+            while (!validOpponent){
+                System.out.println("Enter the opponent you would like to face (1, 2): ");
+                opponent = reader.readLine();
+
+                if (Objects.equals(opponent, "1")
+                        || Objects.equals(opponent, "2")){
+                    validOpponent = true;
+                }
+
+            }
+
+            // Play chosen game.
+            player.setOptions(gameMode);
+            originalComputer.setOptions(gameMode);
+            lastChoiceComputer.setOptions(gameMode);
+
+
         }
     }
 }
