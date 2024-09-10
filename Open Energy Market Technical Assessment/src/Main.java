fcdc097559c76857710f940357d2cbfcbd301580
TobySlump <toby.slump@gmail.com>
2024-09-10 10:08:43 +0100

    Moved menus into functions

--- Open Energy Market Technical Assessment/src/Main.java
+++ Open Energy Market Technical Assessment/src/Main.java
@@ -25,49 +25,15 @@ public class Main {
 
         boolean keepPlaying = true;
         while (keepPlaying){
-            System.out.println("""
-                Menu:\s
-                1) Original Rock Paper Scissors.\s
-                2) Extended Rock Paper Scissors Lizard Spock\s
-                3) Quit""");
-
-            boolean validGameMode = false;
-
-            // Repeat game mode question until valid answer provided.
-            while (!validGameMode) {
-                System.out.println("Enter the game mode you would like to play (1, 2, 3): ");
-                gameMode = reader.readLine();
-
-                if (Objects.equals(gameMode, "1")
-                        || Objects.equals(gameMode, "2")
-                        || Objects.equals(gameMode, "3")) {
-                    validGameMode = true;
-                }
-            }
+            gameMode = chooseGameMode(reader);
 
             // Exit keepPlaying loop preemptively if "Quit" option is chosen.
             if (gameMode.equals("3")){
                 break;
             }
 
-            System.out.println("""
-                Opponent Options:\s
-                1) Random Computer.\s
-                2) Last Choice Computer""");
+            opponent = chooseOpponent(reader);
 
-            boolean validOpponent = false;
-
-            // Repeat opponent question until valid answer provided.
-            while (!validOpponent){
-                System.out.println("Enter the opponent you would like to face (1, 2): ");
-                opponent = reader.readLine();
-
-                if (Objects.equals(opponent, "1")
-                        || Objects.equals(opponent, "2")){
-                    validOpponent = true;
-                }
-
-            }
 
             // Play chosen game.
             player.setOptions(gameMode);
@@ -75,6 +41,58 @@ public class Main {
             lastChoiceComputer.setOptions(gameMode);
 
 
+
         }
+    }
+
+    public String chooseGameMode(BufferedReader reader) throws IOException {
+        String gameMode = null;
+
+        System.out.println("""
+                Menu:\s
+                1) Original Rock Paper Scissors.\s
+                2) Extended Rock Paper Scissors Lizard Spock\s
+                3) Quit""");
+
+        boolean validGameMode = false;
+
+        // Repeat game mode question until valid answer provided.
+        while (!validGameMode) {
+            System.out.println("Enter the game mode you would like to play (1, 2, 3): ");
+            gameMode = reader.readLine();
+
+            if (Objects.equals(gameMode, "1")
+                    || Objects.equals(gameMode, "2")
+                    || Objects.equals(gameMode, "3")) {
+                validGameMode = true;
+            }
+        }
+
+        return gameMode;
+    }
+
+    public String chooseOpponent(BufferedReader reader) throws IOException{
+        String opponent = null;
+
+        System.out.println("""
+                Opponent Options:\s
+                1) Random Computer.\s
+                2) Last Choice Computer""");
+
+        boolean validOpponent = false;
+
+        // Repeat opponent question until valid answer provided.
+        while (!validOpponent){
+            System.out.println("Enter the opponent you would like to face (1, 2): ");
+            opponent = reader.readLine();
+
+            if (Objects.equals(opponent, "1")
+                    || Objects.equals(opponent, "2")){
+                validOpponent = true;
+            }
+
+        }
+
+        return opponent;
     }
 }
